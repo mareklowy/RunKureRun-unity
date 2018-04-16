ccb3f6362c50e010fa5f0c981c1075fe2bb61dae
Marek Löwy <marek.lewy23@gmail.com>
2018-04-16 20:51:47 +0200

    Created project

---
+++ MetinLike/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+	public Transform player;
+	public Transform camera;
+
+	// Update is called once per frame
+	void Update()
+	{
+		Vector3 offset = new Vector3(0f, 1.81f, -2.02f);
+		camera.position = player.position + offset;
+		var cameraRotation = camera.rotation;
+		cameraRotation.y = player.rotation.y - 178.987f;
+	}
+}
